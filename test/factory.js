20439b9c4d7f5c12ed64f74418e792a610df413c
FrancoisGodin <francois.h.godin@gmail.com>
2020-06-18 14:02:45 +0200

    Etape 4 : new feature 'create an User'

--- test/factory.js
+++ test/factory.js
@@ -1,6 +1,22 @@
 const models = require('../lib/models')
+const User = models.User
 
 const factory = {
+    createUserData: ({
+        pseudo = 'Namelistone',
+        password = 'admin123',
+        name = 'Benoit'
+      } = {}) => {
+        return { pseudo, password, name }
+      },
+      createUser: ({
+        id = 756,
+        pseudo = 'Namelistone',
+        password = 'admin123',
+        name = 'Benoit'
+      } = {}) => {
+        return new User({ id, pseudo, password,name })
+      }
 }
 
 module.exports = factory
