56b703e3ab5ebd1f2f1b81f569b69d45e3da883d
Namelistone <namelistone@gmail.com>
2020-06-18 16:59:09 +0200

    Delete

--- test/routes/userRouterTest.js
+++ test/routes/userRouterTest.js
@@ -254,4 +254,37 @@ describe('userRouter', () => {
 
     })
     })
+
+    describe('delete', () => {
+
+      let response
+  
+      beforeEach(() => {
+        sinon.stub(userRepository, 'deleteById')
+      })
+  
+      context('when the id exist in the database', () => {
+  
+          beforeEach(async () => {
+          // given
+          userId = '1000'
+          userRepository.deleteById.resolves()
+  
+          // when
+          response = await request(app).delete('/users/'+userId)
+          })
+  
+          it('should call the user repository', () => {
+              // then
+              expect(userRepository.deleteById).to.have.been.calledWith(userId)
+          })
+  
+          it('should succeed with a status 204', () => {
+          // then
+              expect(response).to.have.status(204)
+              expect(response.text).to.be.empty
+          })
+      })
+  
+    }) 
 })
