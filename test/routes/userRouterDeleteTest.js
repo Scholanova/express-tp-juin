d2f0d5b0a06026f15f8cf407b78c0aede95225e9
Namelistone <namelistone@gmail.com>
2020-06-18 16:22:01 +0200

    Delete

--- test/routes/userRouterDeleteTest.js
+++ test/routes/userRouterDeleteTest.js
@@ -1,9 +1,7 @@
-const Joi = require('@hapi/joi')
 const { expect, request, sinon, factory } = require('../testHelper')
 const { ResourceNotFoundError } = require('../../lib/errors')
 const app = require('../../lib/app')
 const userRepository = require('../../lib/repositories/userRepository')
-const userService = require('../../lib/services/userService')
 const models = require('../../lib/models')
 const User = models.User
 
@@ -17,84 +15,28 @@ describe('userRouterDelete', () => {
       sinon.stub(userRepository, 'deleteById')
     })
 
-    context('when there is no users in the repository', () => {
+    context('when the id exist in the database', () => {
 
-      beforeEach(async () => {
+        beforeEach(async () => {
         // given
-        userRepository.listAll.resolves([])
-
+        userId = '1000'
+        userRepository.deleteById.resolves()
+        
         // when
-        response = await request(app).get('/users')
-      })
-
-      it('should succeed with a status 200', () => {
-        // then
-        expect(response).to.have.status(200)
-      })
-
-      it('should return an empty list message', () => {
-        // then
-        expect(response).to.be.html
-        expect(response.text).to.contain('No users in system yet.')
-      })
-    })
-
-    context('when there are users in the repository', () => {
-
-      let user
-      beforeEach(async () => {
-        // given
-        user = ({
-            name: 'Jean-Jacques Rousseau',
-            password: 'pala',
-            pseudo: 'Bonsoir',
+        response = await request(app).delete('/users/'+userId)
         })
 
-        userRepository.listAll.resolves([user])
-
-        // when
-        response = await request(app).get('/users')
-      })
-
-      it('should succeed with a status 200', () => {
-        // then
-        expect(response).to.have.status(200)
-      })
-
-      it('should return an html list with user info inside', () => {
-        // then
-        expect(response).to.be.html
-        expect(response.text).to.contain(`${user.name} (${user.pseudo})`)
-      })
-    })
-
-    context('when there are users in the repository', () => {
-
-      let user
-      beforeEach(async () => {
-        // given
-        user = ({
-            name: 'Jean-Jacques Rousseau',
-            password: 'pala',
-            pseudo: 'Bonsoir',
+        it('should call the user repository', () => {
+            // then
+            expect(userRepository.deleteById).to.have.been.calledWith(userId)
         })
-        userRepository.listAll.resolves([user])
 
-        // when
-        response = await request(app).get('/users')
-      })
-
-      it('should succeed with a status 200', () => {
+        it('should succeed with a status 204', () => {
         // then
-        expect(response).to.have.status(200)
-      })
-
-      it('should return an html list with user info inside', () => {
-        // then
-        expect(response).to.be.html
-        expect(response.text).to.contain(`${user.name}`)
-      })
+            expect(response).to.have.status(204)
+            expect(response.text).to.be.empty
+        })
     })
-  })
-  
+    
+  }) 
 })
