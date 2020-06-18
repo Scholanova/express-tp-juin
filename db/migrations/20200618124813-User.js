198d876051c8e8e1dec12d5e4fe7ad047780180c
Florian <florianbematol@gmail.com>
2020-06-18 15:48:07 +0200

    Florian - Etape 2

--- db/migrations/20200618124813-User.js
+++ db/migrations/20200618124813-User.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Authors', {
+    return queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
