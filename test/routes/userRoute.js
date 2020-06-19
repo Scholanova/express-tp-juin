4179f515558d27bc7663ada675ca8e93506475c4
elsene <magenelec@gmail.com>
2020-06-19 11:45:25 +0200

    etape 4

---
+++ test/routes/userRoute.js
@@ -0,0 +1,13 @@
+const userRepository = require('../repositories/userRepository')
+
+const router = require('express').Router()
+
+router.get('/', function(req, res, next) {
+    userRepository.listAll()
+        .then((users) => {
+            res.render('user/list', { title: 'Users', users })
+        })
+        .catch(next)
+})
+
+module.exports = router
