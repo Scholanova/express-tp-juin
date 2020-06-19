935cb142e19eb7d900ca02e0a256771627a28f3f
elsene <magenelec@gmail.com>
2020-06-19 14:51:17 +0200

    etape 4

--- test/routes/userRoute.js
+++ test/routes/userRoute.js
@@ -1,13 +1,63 @@
-const userRepository = require('../repositories/userRepository')
+const { expect, request, sinon, factory } = require('../testHelper')
+const app = require('../../lib/app')
+const userRepository = require('../../lib/repositories/userRepository')
 
-const router = require('express').Router()
+describe('userRouter', () => {
 
-router.get('/', function(req, res, next) {
-    userRepository.listAll()
-        .then((users) => {
-            res.render('user/list', { title: 'Users', users })
+    describe('list', () => {
+
+      let response
+
+      beforeEach(() => {
+        sinon.stub(userRepository, 'listAll')
+      })
+
+      context('when there is no users in the repository', () => {
+
+        beforeEach(async () => {
+          // given
+          userRepository.listAll.resolves([])
+
+          // when
+          response = await request(app).get('/users')
         })
-        .catch(next)
-})
 
-module.exports = router
+        it('should succeed with a status 200', () => {
+          // then
+          expect(response).to.have.status(200)
+        })
+
+        it('should return an empty list message', () => {
+          // then
+          expect(response).to.be.html
+          expect(response.text).to.contain('No users in system yet.')
+        })
+      })
+
+      context('when there are users in the repository', () => {
+
+        let user
+        beforeEach(async () => {
+          // given
+          user = factory.createUser()
+          userRepository.listAll.resolves([user])
+
+          // when
+          response = await request(app).get('/users')
+        })
+
+        it('should succeed with a status 200', () => {
+          // then
+          expect(response).to.have.status(200)
+        })
+
+        it('should return an html list with user info inside', () => {
+          // then
+          expect(response).to.be.html
+          expect(response.text).to.contain(`${user.name} ( ${user.email} )`)
+        })
+      })
+
+    })
+
+}) 
